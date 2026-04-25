1c2bb3922681e54ea52d65c87d8652ba8d0ea697
Justin <shpaw415@gmail.com>
2026-04-25 00:23:58 -0400

    tested ok

--- exemple/src/pages/index.tsx
+++ exemple/src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 export default function MainPage() {
+	const [state, setState] = useState(0);
 	return (
 		<div>
-			<h1>Main Page</h1>
+			<h1>Main Page {state}</h1>
+			<button type="button" onClick={() => setState(state + 1)}>
+				Increment
+			</button>
 		</div>
 	);
 }
