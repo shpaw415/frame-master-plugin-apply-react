9dd5514885f4609a912d301f8f685085c82baf50
Justin Halle <shpaw415@gmail.com>
2026-05-14 14:26:04 -0400

    update test env

--- exemple/src/pages/index.tsx
+++ exemple/src/pages/index.tsx
@@ -8,7 +8,19 @@ export default function MainPage() {
 			<button type="button" onClick={() => setState(state + 1)}>
 				Increment
 			</button>
-			<a href="/about">Go to about page</a>
+			<div
+				style={{
+					marginTop: "1rem",
+					display: "flex",
+					flexDirection: "column",
+					gap: "0.5rem",
+				}}
+			>
+				<a href="/about">Go to about page</a>
+				<a href="/sub/123">Go to sub page with id 123</a>
+				<a href="/non-existent">Go to non-existent page</a>
+				<a href="/sub">raw sub page</a>
+			</div>
 		</div>
 	);
 }
